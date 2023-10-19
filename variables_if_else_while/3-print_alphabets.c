137d043916ad212672fcca29594852c2bbc48fb5
xvrchr <cx.rivera3@gmail.com>
2023-10-19 15:59:15 -0400

    Variables 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabetic in Uppercase
+ *
+ * Return: Always (Sucess)
+ */
+int main(void)
+{
+	char c
+
+		for (c = 'A'; c <= 'Z'; c++)
+			putchar(c);
+
+	putchar('\n');
+	return (0);
+}
