e35f61ee3f436a25eb648ba1d4419590d5cacbd2
xvrchr <cx.rivera3@gmail.com>
2023-10-19 16:34:04 -0400

    Variables 4

---
+++ variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Return:Always (Sucess)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+	}
+
+	putchar('\n')
+
+	return (0);
+}
